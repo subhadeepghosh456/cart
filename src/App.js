5baf4ff21d55c97575de8fd673a3c2dfdb7a455b
subhadeepghosh456 <srsubhadeepghosh456@gmail.com>
2022-07-28 20:21:27 +0530

    incomplete

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "react-toastify/dist/utils/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+import BuyPage from './components/BuyPage';
+
+function App() {
+
+  const [cartItem,setCartItem] = useState([]);
+
+  const addInCart = (item) =>{
+    const isAlreadyAdded = cartItem.findIndex(function(array){
+      return array.id === item.id;
+    });
+
+    if(isAlreadyAdded!== -1){
+      toast("already added in cart",{
+        type:"error"
+      })
+    }
+
+    setCartItem([...cartItem,item]);
+
+  };
+
+
+  const buyNow = () => {
+    setCartItem([]);
+
+    toast("Purchase Complete",{
+      type:'success'
+    })
+  }
+
+  const removeItem = item =>{
+    setCartItem(cartItem.filter(singleItem => singleItem.id !== item.id));
+  };
+
+
+  return (
+    <div className="App">
+      <BuyPage addInCart={addInCart}/>
+    </div>
+  );
+}
+
+export default App;
